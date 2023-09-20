a8ae575fb62ba8250662e434e99a4377c29f858d
sapera11 <akinsetewilliams@gmail.com>
2023-09-20 01:49:22 -0700

    updated

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,8 +1,6 @@
--- Setup for MySQL server
--- Script to automate setup
-
+-- Prepares the MySQL server for this project
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-USE hbnb_test_db;
-GRANT ALL ON hbnb_test_db.* TO 'hbnb_test'@'localhost'
-IDENTIFIED BY 'hbnb_test_pwd';
-GRANT SELECT ON performance_schema.* TO hbnb_test@localhost;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
