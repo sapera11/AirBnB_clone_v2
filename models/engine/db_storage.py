6389815705b04409299b8bdde7c95b7ddb1eb74f
teepatra <fagboretomilola@gmail.com>
2023-09-20 06:41:41 -0700

    changed storage engine to DBstorage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -4,7 +4,7 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 import urllib.parse
-
+import models
 from models.base_model import BaseModel, Base
 from models.state import State
 from models.city import City
